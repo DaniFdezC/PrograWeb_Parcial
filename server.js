d54691b4bd558f5f0b9b164e6d1b3028e52c5f4e
DaniFdezC <dani96sk@hotmail.com>
2023-12-05 16:41:11 +0100

    cambio1

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ const db = require("./app/models");
 const Role = db.role;
 
 db.sequelize.sync();
-// force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
@@ -34,20 +29,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
-}
